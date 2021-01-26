a2af80d9a386d7a28e0c22044d5a520f98c23037
el0xren <66721313+el0xren@users.noreply.github.com>
2021-01-26 11:28:16 +0300

    lineage > bliss

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_joyeuse.mk
+    $(LOCAL_DIR)/bliss_joyeuse.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_joyeuse-user \
